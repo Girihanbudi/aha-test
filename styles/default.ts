be3e9623e7ed8e346c4a4d31b88679e61b7ecae2
DESKTOP-FCML9R4\ghanb <ghanbudi@gmail.com>
2022-05-29 22:07:14 +0700

    initial commit

---
+++ styles/default.ts
@@ -0,0 +1,630 @@
+import { createTheme, Palette, PaletteOptions } from '@mui/material/styles'
+
+// COLOR GENERATED FROM
+// http://mcg.mbitson.com/
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    mode: string
+    palette: Palette
+    mainColor: {
+      main: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+      secondary: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+      primary: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+      strong: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+      danger: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+      warning: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+      success: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+      disabled: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+      dissapear: {
+        50: string
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+        A100: string
+        A200: string
+        A400: string
+        A700: string
+        contrastDefaultColor: string
+      }
+    }
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    mode?: string
+    palette?: PaletteOptions | undefined
+    mainColor?: {
+      main?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+      secondary?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+      primary?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+      strong?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+      danger?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+      warning?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+      success?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+      disabled?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+      dissapear?: {
+        50?: string
+        100?: string
+        200?: string
+        300?: string
+        400?: string
+        500?: string
+        600?: string
+        700?: string
+        800?: string
+        900?: string
+        A100?: string
+        A200?: string
+        A400?: string
+        A700?: string
+        contrastDefaultColor?: string
+      }
+    }
+  }
+}
+
+export const lightTheme = createTheme({
+  mode: 'light',
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: '#22333B',
+    },
+    error: {
+      main: '#ce0000',
+    },
+    warning: {
+      main: '#ffd400',
+    },
+    info: {
+      main: '#4064AC',
+    },
+    success: {
+      main: '#006303',
+    },
+  },
+  mainColor: {
+    main: {
+      50: '#e0e0e0',
+      100: '#b3b3b3',
+      200: '#808080',
+      300: '#4d4d4d',
+      400: '#262626',
+      500: '#000000',
+      600: '#000000',
+      700: '#000000',
+      800: '#000000',
+      900: '#000000',
+      A100: '#a6a6a6',
+      A200: '#8c8c8c',
+      A400: '#737373',
+      A700: '#666666',
+      contrastDefaultColor: 'light',
+    },
+    secondary: {
+      50: '#e4e7e7',
+      100: '#bdc2c4',
+      200: '#91999d',
+      300: '#647076',
+      400: '#435258',
+      500: '#22333b',
+      600: '#1e2e35',
+      700: '#19272d',
+      800: '#142026',
+      900: '#0c1419',
+      A100: '#5bc8ff',
+      A200: '#28b7ff',
+      A400: '#00a3f4',
+      A700: '#0092da',
+      contrastDefaultColor: 'light',
+    },
+    primary: {
+      50: '#e2eaf4',
+      100: '#b7c9e3',
+      200: '#87a6d0',
+      300: '#5682bd',
+      400: '#3267af',
+      500: '#0e4ca1',
+      600: '#0c4599',
+      700: '#0a3c8f',
+      800: '#083385',
+      900: '#042474',
+      A100: '#a2b7ff',
+      A200: '#6f8fff',
+      A400: '#3c67ff',
+      A700: '#2353ff',
+      contrastDefaultColor: 'light',
+    },
+    strong: {
+      50: '#e0e2e5',
+      100: '#b3b6bf',
+      200: '#808694',
+      300: '#4d5569',
+      400: '#263049',
+      500: '#000c29',
+      600: '#000a24',
+      700: '#00081f',
+      800: '#000619',
+      900: '#00030f',
+      A100: '#5252ff',
+      A200: '#1f1fff',
+      A400: '#0000eb',
+      A700: '#0000d1',
+      contrastDefaultColor: 'light',
+    },
+    danger: {
+      50: '#fae0e0',
+      100: '#f3b3b3',
+      200: '#ec8080',
+      300: '#e44d4d',
+      400: '#de2626',
+      500: '#d80000',
+      600: '#d40000',
+      700: '#ce0000',
+      800: '#c80000',
+      900: '#bf0000',
+      A100: '#ffe7e7',
+      A200: '#ffb4b4',
+      A400: '#ff8181',
+      A700: '#ff6868',
+      contrastDefaultColor: 'light',
+    },
+    warning: {
+      50: '#fffbe0',
+      100: '#fff5b3',
+      200: '#ffee80',
+      300: '#ffe74d',
+      400: '#ffe226',
+      500: '#ffdd00',
+      600: '#ffd900',
+      700: '#ffd400',
+      800: '#ffcf00',
+      900: '#ffc700',
+      A100: '#ffffff',
+      A200: '#fffcf2',
+      A400: '#ffefbf',
+      A700: '#ffe9a6',
+      contrastDefaultColor: 'dark',
+    },
+    success: {
+      50: '#e0f2e2',
+      100: '#b3deb6',
+      200: '#80c985',
+      300: '#4db354',
+      400: '#26a22f',
+      500: '#00920a',
+      600: '#008a09',
+      700: '#007f07',
+      800: '#007505',
+      900: '#006303',
+      A100: '#93ff93',
+      A200: '#60ff60',
+      A400: '#2dff2d',
+      A700: '#14ff14',
+      contrastDefaultColor: 'light',
+    },
+    disabled: {
+      50: '#fbfbfb',
+      100: '#f5f5f5',
+      200: '#eeeeee',
+      300: '#e7e7e7',
+      400: '#e1e1e1',
+      500: '#dcdcdc',
+      600: '#d8d8d8',
+      700: '#d3d3d3',
+      800: '#cecece',
+      900: '#c5c5c5',
+      A100: '#ffffff',
+      A200: '#ffffff',
+      A400: '#ffffff',
+      A700: '#ffffff',
+      contrastDefaultColor: 'dark',
+    },
+  },
+})
+
+// export const darkTheme = createTheme({
+//   mode: 'dark',
+//   mainColor: {
+//     main: {
+//       50: '#e6e6e6',
+//       100: '#c1c1c1',
+//       200: '#989898',
+//       300: '#6e6e6e',
+//       400: '#4f4f4f',
+//       500: '#303030',
+//       600: '#2b2b2b',
+//       700: '#242424',
+//       800: '#1e1e1e',
+//       900: '#131313',
+//       A100: '#ef6c6c',
+//       A200: '#ea3e3e',
+//       A400: '#f50000',
+//       A700: '#db0000',
+//       contrastDefaultColor: 'light',
+//     },
+//     secondary: {
+//       50: '#f6f6f6',
+//       100: '#e9e9e9',
+//       200: '#dadada',
+//       300: '#cbcbcb',
+//       400: '#c0c0c0',
+//       500: '#b5b5b5',
+//       600: '#aeaeae',
+//       700: '#a5a5a5',
+//       800: '#9d9d9d',
+//       900: '#8d8d8d',
+//       A100: '#ffffff',
+//       A200: '#fef4f4',
+//       A400: '#ffc0c0',
+//       A700: '#ffa7a7',
+//       contrastDefaultColor: 'dark',
+//     },
+//     primary: {
+//       50: '#e2eaf4',
+//       100: '#b7c9e3',
+//       200: '#87a6d0',
+//       300: '#5682bd',
+//       400: '#3267af',
+//       500: '#0e4ca1',
+//       600: '#0c4599',
+//       700: '#0a3c8f',
+//       800: '#083385',
+//       900: '#042474',
+//       A100: '#a2b7ff',
+//       A200: '#6f8fff',
+//       A400: '#3c67ff',
+//       A700: '#2353ff',
+//       contrastDefaultColor: 'light',
+//     },
+//     strong: {
+//       50: '#e0e2e5',
+//       100: '#b3b6bf',
+//       200: '#808694',
+//       300: '#4d5569',
+//       400: '#263049',
+//       500: '#000c29',
+//       600: '#000a24',
+//       700: '#00081f',
+//       800: '#000619',
+//       900: '#00030f',
+//       A100: '#5252ff',
+//       A200: '#1f1fff',
+//       A400: '#0000eb',
+//       A700: '#0000d1',
+//       contrastDefaultColor: 'light',
+//     },
+//     danger: {
+//       50: '#fae0e0',
+//       100: '#f3b3b3',
+//       200: '#ec8080',
+//       300: '#e44d4d',
+//       400: '#de2626',
+//       500: '#d80000',
+//       600: '#d40000',
+//       700: '#ce0000',
+//       800: '#c80000',
+//       900: '#bf0000',
+//       A100: '#ffe7e7',
+//       A200: '#ffb4b4',
+//       A400: '#ff8181',
+//       A700: '#ff6868',
+//       contrastDefaultColor: 'light',
+//     },
+//     warning: {
+//       50: '#fffbe0',
+//       100: '#fff5b3',
+//       200: '#ffee80',
+//       300: '#ffe74d',
+//       400: '#ffe226',
+//       500: '#ffdd00',
+//       600: '#ffd900',
+//       700: '#ffd400',
+//       800: '#ffcf00',
+//       900: '#ffc700',
+//       A100: '#ffffff',
+//       A200: '#fffcf2',
+//       A400: '#ffefbf',
+//       A700: '#ffe9a6',
+//       contrastDefaultColor: 'dark',
+//     },
+//     success: {
+//       50: '#e0f2e2',
+//       100: '#b3deb6',
+//       200: '#80c985',
+//       300: '#4db354',
+//       400: '#26a22f',
+//       500: '#00920a',
+//       600: '#008a09',
+//       700: '#007f07',
+//       800: '#007505',
+//       900: '#006303',
+//       A100: '#93ff93',
+//       A200: '#60ff60',
+//       A400: '#2dff2d',
+//       A700: '#14ff14',
+//       contrastDefaultColor: 'light',
+//     },
+//     disabled: {
+//       50: '#fbfbfb',
+//       100: '#f5f5f5',
+//       200: '#eeeeee',
+//       300: '#e7e7e7',
+//       400: '#e1e1e1',
+//       500: '#dcdcdc',
+//       600: '#d8d8d8',
+//       700: '#d3d3d3',
+//       800: '#cecece',
+//       900: '#c5c5c5',
+//       A100: '#ffffff',
+//       A200: '#ffffff',
+//       A400: '#ffffff',
+//       A700: '#ffffff',
+//       contrastDefaultColor: 'dark',
+//     },
+//   },
+// })
